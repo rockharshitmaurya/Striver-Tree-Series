57e6d01c528514175277db48f342f13fff0b4ba3
Harshit Maurya <rockharshitmaurya@gmail.com>
2022-01-08 16:35:47 +0530

    Preorder Traversal of Tree

---
+++ PreorderTraversal.java
@@ -0,0 +1,31 @@
+class Node{
+    int data;
+    Node left;
+    Node right;
+    public Node(int key){
+        this.data=key;
+    }
+}
+class PreorderTraversal{
+    public static void main(String[] args) {
+        Node root=new Node(1);
+        root.left=new Node(2);
+        root.left.left=new Node(4);
+        root.left.right=new Node(5);
+        root.left.right.left=new Node(8);
+        root.right=new Node(3);
+        root.right.left=new Node(6);
+        root.right.right=new Node(7);
+        root.right.right.left=new Node(9);
+        root.right.right.right=new Node(10);
+        inorder(root);
+    }
+    public static void inorder(Node root){
+        if(root==null){
+            return;
+        }
+        System.out.print(root.data+" ");
+        inorder(root.left);
+        inorder(root.right);
+    }
+}
