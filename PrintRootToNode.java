be62962a7945d6909c909a3fa5964bea1bf8aacb
Harshit Maurya <rockharshitmaurya@gmail.com>
2022-01-16 20:27:25 +0530

    Print Root to Any Given Node

---
+++ PrintRootToNode.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for binary tree
+ * class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) {
+ *      val = x;
+ *      left=null;
+ *      right=null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ArrayList<Integer> solve(TreeNode A, int B) {
+        ArrayList<Integer> arr=new ArrayList<>();
+        if(A==null) return arr;
+        getPath(A,arr,B);
+        return arr;
+    }
+    boolean getPath(TreeNode root,ArrayList<Integer> arr,int B){
+        if(root==null) return false;
+        arr.add(root.val);
+        if(root.val==B) return true;
+        if(getPath(root.left, arr,B) || getPath(root.right,arr,B)) return true; 
+        arr.remove(arr.size()-1);
+        return false;
+    }
+
+}
