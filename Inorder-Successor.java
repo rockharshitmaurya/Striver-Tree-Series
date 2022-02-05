dbdabcf2f807567b816bc23341863688c9d2b8ff
Harshit Maurya <rockharshitmaurya@gmail.com>
2022-02-05 14:30:30 +0530

    Inorder Successor in BST

---
+++ Inorder-Successor.java
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/inorder-successor-in-bst/
+class Solution {
+    
+    public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+        
+        TreeNode successor = null;
+        
+        while (root != null) {
+            
+            if (p.val >= root.val) {
+                root = root.right;
+            } else {
+                successor = root;
+                root = root.left;
+            }
+        }
+        
+        return successor;
+    }
+}
